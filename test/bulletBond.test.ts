0327aff2ebc7357fbbba3e9efaa5fa0e41bcfa55
qpzm <lhm1442@gmail.com>
2022-08-24 21:35:39 +0900

    [EUP-57] Add bulletBond.test.ts

--- test/bulletBond.test.ts
+++ test/bulletBond.test.ts
@@ -83,7 +83,7 @@ describe("BulletBond", function () {
       const { bulletBond, usdt } = await loadFixture(deployBulletBond);
       await addProduct(bulletBond, usdt);
 
-      const finalValue = ethers.utils.parseEther("100");
+      const finalValue = ethers.utils.parseEther("115");
       const totalFinalValue = finalValue.mul(1000);
 
       await usdt.approve(bulletBond.address, totalFinalValue);
@@ -97,7 +97,7 @@ describe("BulletBond", function () {
       const { bulletBond, usdt } = await loadFixture(deployBulletBond);
       await addProduct(bulletBond, usdt);
 
-      const finalValue = ethers.utils.parseEther("100");
+      const finalValue = ethers.utils.parseEther("115");
       const totalFinalValue = finalValue.mul(1000);
       await usdt.approve(bulletBond.address, totalFinalValue);
 
@@ -108,23 +108,157 @@ describe("BulletBond", function () {
   });
 
   describe("#claim", function () {
-    it("should allow users with zero balance to claim", async function () {
-      const { bulletBond } = await loadFixture(deployBulletBond);
+    it("should revert when the project is not repaid", async function () {
+      const { bulletBond, usdt, alice } = await loadFixture(deployBulletBond);
+      const id = 0;
+      await expect(
+        bulletBond.claim(alice.address, id)
+      ).to.revertedWithCustomError(bulletBond, "NotRepaid");
     });
 
-    it("should allow users to claim multiple times", async function () {
-      const { bulletBond } = await loadFixture(deployBulletBond);
+    it("should revert when a user with zero balance claims", async function () {
+      // Setup
+      const { bulletBond, usdt, alice } = await loadFixture(deployBulletBond);
+      await addProduct(bulletBond, usdt);
+      const id = 0;
+
+      const finalValue = ethers.utils.parseEther("115");
+      const totalFinalValue = finalValue.mul(1000);
+
+      await usdt.approve(bulletBond.address, totalFinalValue);
+      await bulletBond.repay(id, finalValue, totalFinalValue);
+
+      // Check
+      await expect(
+        bulletBond.claim(alice.address, id)
+      ).to.revertedWithCustomError(bulletBond, "ZeroBalanceClaim");
     });
 
     context("when repaid", function () {
-      it("should transfer principal + unclaimed interest at first", async function () {});
-      it("should transfer 0 at second", async function () {});
+      let testEnv: any = {};
+
+      beforeEach(async function () {
+        const { bulletBond, usdt, owner, alice } = await loadFixture(
+          deployBulletBond
+        );
+        await addProduct(bulletBond, usdt);
+        const id = 0;
+        const amount = 33;
+
+        const finalValue = ethers.utils.parseEther("115");
+        const totalFinalValue = finalValue.mul(1000);
+
+        await usdt.approve(bulletBond.address, totalFinalValue);
+        await bulletBond.repay(id, finalValue, totalFinalValue);
+
+        await bulletBond.safeTransferFrom(
+          owner.address,
+          alice.address,
+          id,
+          amount,
+          []
+        );
+
+        testEnv.bulletBond = bulletBond;
+        testEnv.usdt = usdt;
+        testEnv.owner = owner;
+        testEnv.alice = alice;
+      });
+
+      it("should burn _to's all nfts", async function () {
+        const { bulletBond, alice } = testEnv;
+        const id = 0;
+
+        await expect(bulletBond.claim(alice.address, id));
+
+        expect(await bulletBond.balanceOf(alice.address, id)).to.eq(0);
+      });
+
+      it("should transfer principal + unclaimed interest at first", async function () {
+        const { bulletBond, usdt, alice } = testEnv;
+        const id = 0;
+
+        await expect(bulletBond.claim(alice.address, id)).to.changeTokenBalance(
+          usdt,
+          alice.address,
+          ethers.utils.parseEther("115").mul(33)
+        );
+      });
+
+      it("should revert at second", async function () {
+        const { bulletBond, usdt, alice } = testEnv;
+        const id = 0;
+
+        await bulletBond.claim(alice.address, id);
+        await expect(
+          bulletBond.claim(alice.address, id)
+        ).to.revertedWithCustomError(bulletBond, "ZeroBalanceClaim");
+      });
     });
   });
 
   describe("#withdrawResidue", function () {
-    it("should allow users with zero balance to claim", async function () {
-      const { bulletBond } = await loadFixture(deployBulletBond);
+    let testEnv: any = {};
+
+    beforeEach(async function () {
+      const { bulletBond, usdt, owner, alice } = await loadFixture(
+        deployBulletBond
+      );
+      await addProduct(bulletBond, usdt);
+      const id = 0;
+      const amount = 33;
+
+      const finalValue = ethers.utils.parseEther("115");
+      const totalFinalValue = finalValue.mul(1000);
+
+      await usdt.approve(bulletBond.address, totalFinalValue);
+      await bulletBond.repay(id, finalValue, totalFinalValue);
+
+      await bulletBond.safeTransferFrom(
+        owner.address,
+        alice.address,
+        id,
+        amount,
+        []
+      );
+
+      await bulletBond.claim(alice.address, id);
+
+      testEnv.bulletBond = bulletBond;
+      testEnv.usdt = usdt;
+      testEnv.owner = owner;
+      testEnv.alice = alice;
+    });
+
+    it("should withdraw remaining stablecoins", async function () {
+      const { bulletBond, alice } = testEnv;
+      const id = 0;
+      await expect(
+        bulletBond.connect(alice).withdrawResidue(id)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("should withdraw remaining stablecoins", async function () {
+      const { bulletBond, usdt, owner } = testEnv;
+      const id = 0;
+      const product = await bulletBond.products(id);
+      const remainingAmount = 967; // 1000 - 33
+
+      await expect(bulletBond.withdrawResidue(id)).to.changeTokenBalance(
+        usdt,
+        owner,
+        product.finalValue.mul(remainingAmount)
+      );
+    });
+
+    it("should not burn unclaimed nfts", async function () {
+      const { bulletBond, owner } = testEnv;
+      const id = 0;
+      const remainingAmount = 967; // 1000 - 33
+      await bulletBond.withdrawResidue(id);
+      expect(await bulletBond.balanceOf(owner.address, id)).to.eq(
+        remainingAmount
+      );
     });
   });
 });
